371b90c4f3ff6d5db7a6985679fc0ca7b9a2cd9c
anissefiani <anis.sefiani@icloud.com>
2020-07-26 14:43:52 +0100

    Updates

---
+++ precache-manifest.63ef287f032c696fb8f4bd4e63e59213.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804036f9106c32a88bc821235d99e625",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "afbd5b3e9de2fada9040",
+    "url": "/facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cc9e356bc14f86378a86",
+    "url": "/facerecognitionbrain/static/css/main.fa9927f6.chunk.css"
+  },
+  {
+    "revision": "afbd5b3e9de2fada9040",
+    "url": "/facerecognitionbrain/static/js/2.9d09e452.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionbrain/static/js/2.9d09e452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9e356bc14f86378a86",
+    "url": "/facerecognitionbrain/static/js/main.3e45c95c.chunk.js"
+  },
+  {
+    "revision": "a0d072c450e7e6f545a0",
+    "url": "/facerecognitionbrain/static/js/runtime-main.3b236050.js"
+  }
+]);
